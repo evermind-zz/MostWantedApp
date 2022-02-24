76f416b78b5e533a416ebccc3b934f9a525f8490
evermind <evermind@tuxfamily.org>
2022-02-25 00:56:52 +0100

    testing here
    - what
    - can
    - we
    - do
    - another 22
    - another 23
    - another 24
    - another 25
    - another 26
    - another 27
    - another 28
    - another 29
    - another 30
    - another 31
    - another 32
    - another 33
    - another 34
    - another 35
    - another 36
    - another 37
    - another 38
    - another 39
    - another 40
    - another 41
    - another 42
    - another 43

--- .github/brave-new-pipe-releast-actions.sh
+++ .github/brave-new-pipe-releast-actions.sh
@@ -36,6 +36,7 @@ create_tagged_release() {
   # Set the local git identity
   git config user.email "${GITHUB_LOGIN}@users.noreply.github.com"
   git config user.name "$GITHUB_LOGIN"
+date
 
   # Obtain the release ID for the previous release of $TAG (if present)
   local previous_release_id=$(curl --user ${GITHUB_LOGIN}:${GITHUB_SUPER_TOKEN} --request GET --silent https://api.github.com/repos/${GITHUB_LOGIN}/${REPO}/releases/tags/${TAG} | jq '.id')
@@ -49,6 +50,7 @@ create_tagged_release() {
       --silent \
       https://api.github.com/repos/${GITHUB_LOGIN}/${REPO}/releases/${previous_release_id}
   fi
+date
 
   # Delete previous identical tags, if present
   git tag -d $TAG || true
@@ -60,33 +62,35 @@ create_tagged_release() {
   git push -f origin master:master
   git tag $TAG
   git push origin $TAG
+date
 
-  # Generate a skeleton release on GitHub
-  curl \
-    --user ${GITHUB_LOGIN}:${GITHUB_SUPER_TOKEN} \
-    --request POST \
-    --silent \
-    --data @- \
-    https://api.github.com/repos/${GITHUB_LOGIN}/${REPO}/releases <<END
-  {
-    "tag_name": "$TAG",
-    "name": "Auto-generated release for tag $TAG",
-    "body": "$RELEASE_BODY",
-    "draft": false,
-    "prerelease": $PRERELEASE
-  }
-END
+# evermind -- we don't want any release entries there  # Generate a skeleton release on GitHub
+# evermind -- we don't want any release entries there  curl \
+# evermind -- we don't want any release entries there    --user ${GITHUB_LOGIN}:${GITHUB_SUPER_TOKEN} \
+# evermind -- we don't want any release entries there    --request POST \
+# evermind -- we don't want any release entries there    --silent \
+# evermind -- we don't want any release entries there    --data @- \
+# evermind -- we don't want any release entries there    https://api.github.com/repos/${GITHUB_LOGIN}/${REPO}/releases <<END
+# evermind -- we don't want any release entries there  {
+# evermind -- we don't want any release entries there    "tag_name": "$TAG",
+# evermind -- we don't want any release entries there    "name": "Auto-generated release for tag $TAG",
+# evermind -- we don't want any release entries there    "body": "$RELEASE_BODY",
+# evermind -- we don't want any release entries there    "draft": false,
+# evermind -- we don't want any release entries there    "prerelease": $PRERELEASE
+# evermind -- we don't want any release entries there  }
+# evermind -- we don't want any release entries thereEND
   popd
 }
 
 URL="https://github.com/bravenewpipe/NewPipe/releases/download/${TAG}/BraveNewPipe_${TAG}.apk"
 VERSION_NAME=${TAG/v/} 
-BUILD_TOOLS_VERSION=${BUILD_TOOLS_VERSION:-'29.0.3'}
-AAPT=$ANDROID_HOME/build-tools/$BUILD_TOOLS_VERSION/aapt
-find $ANDROID_HOME -name 'aapt'
-env
-VERSION_CODE="$($AAPT d badging $APK_FILE | grep -Po "(?<=\sversionCode=')([0-9.-]+)")"
+date
 
+BUILD_TOOLS_VERSION="${BUILD_TOOLS_VERSION:-29.0.3}"
+AAPT=$ANDROID_HOME/build-tools/$BUILD_TOOLS_VERSION/aapt
+
+VERSION_CODE="$($AAPT d badging $APK_FILE | grep -Po "(?<=\sversionCode=')([0-9.-]+)")"
+date
 TEMPFILE="$(mktemp  -p $PWD -t sdflhXXXXXXXXX)"
 JSON_FILE=data.json
 
@@ -94,12 +98,16 @@ JSON_FILE=data.json
 rm -rf "/tmp/${BNP_R_MGR_REPO}"
 git clone "https://evermind-zz:${GITHUB_SUPER_TOKEN}@github.com/evermind-zz/${BNP_R_MGR_REPO}.git" /tmp/${BNP_R_MGR_REPO}
 # update the json file
+date
 cat /tmp/${BNP_R_MGR_REPO}/api/$JSON_FILE \
     | jq '.flavors.github.stable.version_code = '${VERSION_CODE}'' \
     | jq '.flavors.github.stable.version = "'${VERSION_NAME}'"' \
     | jq '.flavors.github.stable.apk = "'${URL}'"' \
     > $TEMPFILE
+date
 
 mv $TEMPFILE /tmp/${BNP_R_MGR_REPO}/api/$JSON_FILE
+date
 create_tagged_release "$BNP_R_MGR_REPO" "\"version\": \"$VERSION_NAME\""
+date
 
